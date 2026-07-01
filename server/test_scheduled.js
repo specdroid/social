2a73b857c4dc94bf1f6e0fa24a0aade8a9313730
Ahmad <ahmad.zeineddine@hotmail.com>
2026-07-01 22:54:42 +0300

    chore: remove test file

--- server/test_scheduled.js
+++
@@ -1,8 +0,0 @@
-const { PrismaClient } = require('@prisma/client')
-const p = new PrismaClient()
-async function test() {
-  try { const r = await p.scheduledPost.findMany(); console.log('ok:', r.length) }
-  catch(e) { console.log('err:', e.message.substring(0,200)) }
-  finally { p.$disconnect() }
-}
-test()
