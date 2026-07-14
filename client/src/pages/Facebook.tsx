a388dd8ee807ff294fd9da0ab11e8b1f5e664913
Ahmad <ahmad.zeineddine@hotmail.com>
2026-07-15 00:30:30 +0300

    feat: remove facebook cookie login setup from UI

--- client/src/pages/Facebook.tsx
+++ client/src/pages/Facebook.tsx
@@ -1,10 +1,8 @@
 import { FacebookPageManager } from '../components/FacebookPageManager'
-import { FacebookWallSetup } from '../components/FacebookWallSetup'
 
 export function Facebook() {
   return (
     <div className="space-y-8">
-      <FacebookWallSetup />
       <FacebookPageManager />
     </div>
   )
